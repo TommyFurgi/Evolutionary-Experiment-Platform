a2caa9d936ad8f162ae01e62afa180b11f97d918
Wiktor Dybalski <wdybalski@student.agh.edu.pl>
2024-11-29 20:02:11 +0100

    Initial Commit

---
+++ src/test/java/com/example/Endpoint_Explorers/EndpointExplorersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Endpoint_Explorers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointExplorersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
